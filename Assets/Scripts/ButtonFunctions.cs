eccfb5a76ea12950d40be70222d8bde86efbdb66
boerdler <robin.brunner@tuta.io>
2021-03-09 17:38:20 +0100

    Initial commit.

---
+++ Assets/Scripts/ButtonFunctions.cs
@@ -0,0 +1,41 @@
+﻿using System.IO;
+using UnityEngine;
+
+public class ButtonFunctions : MonoBehaviour
+{
+    public Transform logTransform;
+    public string filepath;
+    StreamWriter file;
+    private void Start()
+    {
+        filepath = Application.dataPath + "/Logs/";
+        if (!Directory.Exists(filepath))
+        {
+            Directory.CreateDirectory(filepath);
+        }        
+        file = new StreamWriter(filepath + "TransformLog.txt", append: true);      
+    }
+    public void Log()
+    {
+        if(logTransform != null)
+        {
+            Vector3 pos = logTransform.position;
+            Vector3 rot = logTransform.eulerAngles;
+
+            Debug.Log("Position: " + pos.x + ", " + pos.y + ", " + pos.z + "; Rotation: " + rot.x + ", " + rot.y + ", " + rot.z);
+
+
+            file.WriteLine(pos.x + "\t" + rot.x);
+            file.WriteLine(pos.y + "\t" + rot.y);
+            file.WriteLine(pos.z + "\t" + rot.z);
+        }    
+    }
+    public void Exit()
+    {
+        Application.Quit();
+    }
+    private void OnDestroy()
+    {
+        file.Close();
+    }
+}
---
+++ Assets/Scripts/TextMeshFaceToCamera.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class TextMeshFaceToCamera : MonoBehaviour
+{
+    void Update()
+    {
+        if(Camera.current)
+            transform.rotation = Quaternion.LookRotation(transform.position - Camera.current.transform.position);
+    }
+}
